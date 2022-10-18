538b9419c54df2a1a4edd2eb4a6fb9d19d8bd363
Nikhil Rikhye <nikhil.rikhye22@imperial.ac.uk>
2022-10-18 23:50:39 +0100

    Question 2 Complete

--- main.cpp
+++ main.cpp
@@ -94,21 +94,61 @@ int main() {
 
 
 bool make_move(const char *position, const int digit, char board[9][9]){
+  // Checks for validity of position input
+  // Checks if string is correct length
   if (strlen(position) != 2)
     return false;
 
-  if (*position < 65 || *position > 73)
-    return false;
+  int input_row_array_number = (*position - '@') - 1;
+  int input_column_array_number = ((*(position + 1) - '0') - 1);
+  int input_digit = digit - '0';
+
   
-  if (*(position + 1) < 49 || *(position + 1) > 57)
+  // Checks if row is within parameters
+  if (input_row_array_number < 0 || input_row_array_number > 8)
     return false;
 
-  cout << static_cast<int>(*position - '@');
-  cout << static_cast<int>(*(position + 1) - '0');
+  // Checks if column is within parameters
+  if (input_column_array_number < 0 || input_column_array_number > 8)
+    return false;
 
-  if (contains_num(board, (*position - '@') - 1, (*(position + 1) - '0') - 1) == true)
+  // Checks if position is already filled in
+  if (contains_num(board, input_row_array_number, input_column_array_number) == true)
       return false;
 
+
+
+  // Checks for validity of digit input
+  // Checks if digit is within parameters
+  if (input_digit < 1)
+    return false;
+
+
+
+  // Checks if move is logically correct
+  // Checks if number already exists in column
+  for (int row = 0; row < 9; row++)
+    if (board[row][input_column_array_number] - '0' == input_digit)
+      return false;
+ 
+  // Checks if number already exists in row
+  for (int col = 0; col < 9; col++)
+    if (board[input_row_array_number][col] - '0' == input_digit)
+      return false;
+
+  // Checks if number already exists in box
+  int start_row = input_row_array_number - input_row_array_number % 3, start_col = input_column_array_number - input_column_array_number % 3;
+
+  for (int row = 0; row < 3; row++)
+    for (int col = 0; col < 3; col++)
+      if (board[row + start_row][col + start_col] - '0' == input_digit)
+	return false;
+  
+
+  // Updates board
+  board[input_row_array_number][input_column_array_number] = digit;
+
+  
   return true;
 }
   
